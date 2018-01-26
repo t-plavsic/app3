7d51be5bbde7ff20d96190785d65865b3837fde6
t-plavsic <tihomir.plavsic@gmail.com>
2018-01-26 19:18:03 +0100

    26/01/2018//amb

---
+++ npm-run-devstart.bat
@@ -0,0 +1,7 @@
+
+cd "C:\Users\Doktor\Desktop\app3"
+
+npm run devstart
+
+pause
+
--- routes/home.js
+++ routes/home.js
@@ -3,14 +3,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('./home_get.njk', {title: 'HOME PAGE'});
+    res.render('./home_get.njk', {loged: false, title: 'HOME-PAGE'});
 });
 
- 
-
 
  
- 
-
-
 module.exports = router;
